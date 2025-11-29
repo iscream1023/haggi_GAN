3f6c62e602fee8ccf0d653d33f1673a6135052e0
본인~ <haggi7289@gmail.com>
2025-11-29 15:23:29 +0900

    2025.11.29

---
+++ GAN_tuto.py
@@ -0,0 +1,245 @@
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")   # 완전 비인터랙티브, 파일 렌더용
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # 입력데이터 Z가 가장 처음 통과하는 전치 합성곱 계층입니다.
+            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # 위의 계층을 통과한 데이터의 크기. ``(ngf*8) x 4 x 4``
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # 위의 계층을 통과한 데이터의 크기. ``(ngf*4) x 8 x 8``
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # 위의 계층을 통과한 데이터의 크기. ``(ngf*2) x 16 x 16``
+            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # 위의 계층을 통과한 데이터의 크기. ``(ngf) x 32 x 32``
+            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # 위의 계층을 통과한 데이터의 크기. ``(nc) x 64 x 64``
+        )
+
+    def forward(self, input):
+        return self.main(input)
+class Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # 입력 데이터의 크기는 ``(nc) x 64 x 64`` 입니다
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 위의 계층을 통과한 데이터의 크기. ``(ndf) x 32 x 32``
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 위의 계층을 통과한 데이터의 크기. ``(ndf*2) x 16 x 16``
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 위의 계층을 통과한 데이터의 크기. ``(ndf*4) x 8 x 8``
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 위의 계층을 통과한 데이터의 크기. ``(ndf*8) x 4 x 4``
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+myseed=999
+random.seed(myseed)
+torch.manual_seed(myseed)
+torch.use_deterministic_algorithms(True)
+
+dataroot = r"/home/ase/haggi_GAN_data/celebA"
+workers = 4
+batch_size = 128
+image_size = 64
+nc = 3
+nz = 100
+ngf = 64
+ndf = 64
+num_epoch = 5
+lr = 2e-4
+beta = 5e-1
+ngpu = 1
+
+dataset = dset.ImageFolder(root=dataroot,
+                           transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+# dataloader를 정의해봅시다
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                         shuffle=True, num_workers=workers)
+
+# GPU 사용여부를 결정해 줍니다
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+# 학습 데이터들 중 몇가지 이미지들을 화면에 띄워봅시다
+
+real_batch = next(iter(dataloader))
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+plt.savefig("plot.png")
+
+# 생성자를 만듭니다
+netG = Generator(ngpu).to(device)
+netD = Discriminator(ngpu).to(device)
+
+# 필요한 경우 multi-GPU를 설정 해주세요
+if (device.type == 'cuda') and (ngpu > 1):
+    netG = nn.DataParallel(netG, list(range(ngpu)))
+    netD = nn.DataParallel(netD, list(range(ngpu)))
+
+# 모든 가중치의 평균을 0( ``mean=0`` ), 분산을 0.02( ``stdev=0.02`` )로 초기화하기 위해
+# ``weight_init`` 함수를 적용시킵니다
+netG.apply(weights_init)
+netD.apply(weights_init)
+
+criterion = nn.BCELoss()
+fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+real_label = 1.
+fake_label = 0.
+
+optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta, 0.999))
+img_list = []
+G_losses = []
+D_losses = []
+iters = 0
+
+print("Starting Training Loop...")
+# 에폭(epoch) 반복
+for epoch in range(num_epoch):
+    # 한 에폭 내에서 배치 반복
+    for i, data in enumerate(dataloader, 0):
+
+        ############################
+        # (1) D 신경망을 업데이트 합니다: log(D(x)) + log(1 - D(G(z)))를 최대화 합니다
+        ###########################
+        ## 진짜 데이터들로 학습을 합니다
+        netD.zero_grad()
+        # 배치들의 사이즈나 사용할 디바이스에 맞게 조정합니다
+        real_cpu = data[0].to(device)
+        b_size = real_cpu.size(0)
+        label = torch.full((b_size,), real_label,
+                           dtype=torch.float, device=device)
+        # 진짜 데이터들로 이루어진 배치를 D에 통과시킵니다
+        output = netD(real_cpu).view(-1)
+        # 손실값을 구합니다
+        errD_real = criterion(output, label)
+        # 역전파의 과정에서 변화도를 계산합니다
+        errD_real.backward()
+        D_x = output.mean().item()
+
+        ## 가짜 데이터들로 학습을 합니다
+        # 생성자에 사용할 잠재공간 벡터를 생성합니다
+        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        # G를 이용해 가짜 이미지를 생성합니다
+        fake = netG(noise)
+        label.fill_(fake_label)
+        # D를 이용해 데이터의 진위를 판별합니다
+        output = netD(fake.detach()).view(-1)
+        # D의 손실값을 계산합니다
+        errD_fake = criterion(output, label)
+        # 역전파를 통해 변화도를 계산합니다. 이때 앞서 구한 변화도에 더합니다(accumulate)
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        # 가짜 이미지와 진짜 이미지 모두에서 구한 손실값들을 더합니다
+        # 이때 errD는 역전파에서 사용되지 않고, 이후 학습 상태를 리포팅(reporting)할 때 사용합니다
+        errD = errD_real + errD_fake
+        # D를 업데이트 합니다
+        optimizerD.step()
+
+        ############################
+        # (2) G 신경망을 업데이트 합니다: log(D(G(z)))를 최대화 합니다
+        ###########################
+        netG.zero_grad()
+        label.fill_(real_label)  # 생성자의 손실값을 구하기 위해 진짜 라벨을 이용할 겁니다
+        # 우리는 방금 D를 업데이트했기 때문에, D에 다시 가짜 데이터를 통과시킵니다.
+        # 이때 G는 업데이트되지 않았지만, D가 업데이트 되었기 때문에 앞선 손실값가 다른 값이 나오게 됩니다
+        output = netD(fake).view(-1)
+        # G의 손실값을 구합니다
+        errG = criterion(output, label)
+        # G의 변화도를 계산합니다
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        # G를 업데이트 합니다
+        optimizerG.step()
+
+        # 훈련 상태를 출력합니다
+        if i % 50 == 0:
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                  % (epoch, num_epoch, i, len(dataloader),
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+        # 이후 그래프를 그리기 위해 손실값들을 저장해둡니다
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+
+        # fixed_noise를 통과시킨 G의 출력값을 저장해둡니다
+        if (iters % 500 == 0) or ((epoch == num_epoch-1) and (i == len(dataloader)-1)):
+            with torch.no_grad():
+                fake = netG(fixed_noise).detach().cpu()
+            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+        iters += 1
+    plt.imshow(np.transpose(vutils.make_grid(img_list[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+    plt.savefig(f"created{epoch}.png")
+    plt.close()
+
+real_batch = next(iter(dataloader))
+
+# 진짜 이미지들을 화면에 출력합니다
+plt.figure(figsize=(15,15))
+plt.subplot(1,2,1)
+plt.axis("off")
+plt.title("Real Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
+
+# 가짜 이미지들을 화면에 출력합니다
+plt.subplot(1,2,2)
+plt.axis("off")
+plt.title("Fake Images")
+plt.savefig("result.png")
+plt.close()
